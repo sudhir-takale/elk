3785ecec3ee9a1917809f5a270714e855ebaac8f
Sudhir Takale <sudhirtakale99@gmail.com>
2025-06-27 10:14:13 +0530

    without apm

--- ObservabilityDemoApplication/start.sh
+++ ObservabilityDemoApplication/start.sh
@@ -5,13 +5,6 @@ mkdir -p /app/logs
 
 /elastic-agent/elastic-agent run -c /elastic-agent.yml > /var/log/elastic-agent/elastic-agent.log 2>&1 &
 
-sleep 5
+sleep 10
 
-exec java \
-  -javaagent:/elastic-apm-agent.jar \
-  -Delastic.apm.service_name=spring-app \
-  -Delastic.apm.environment=prod \
-  -Delastic.apm.application_packages=com.example.observability \
-  -Delastic.apm.server_urls=http://3.109.139.197:8200 \
-  -Delastic.apm.log_level=INFO \
-  -jar /app/app.jar
+exec java -jar /app/app.jar
