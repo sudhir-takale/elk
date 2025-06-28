3bfbc663addbddb71844ca6ffa7780275d672a19
Sudhir Takale <sudhirtakale99@gmail.com>
2025-06-28 22:07:35 +0530

    everythign works fine getting application logs also

--- ObservabilityDemoApplication/start.sh
+++ ObservabilityDemoApplication/start.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 set -e
 
@@ -7,4 +8,13 @@ mkdir -p /app/logs
 
 sleep 10
 
-exec java -jar /app/app.jar
+exec java \
+  -javaagent:/elastic-apm-agent.jar \
+  -Delastic.apm.server_urls=http://65.0.76.204:8200 \
+  -Delastic.apm.service_name=spring-app \
+  -Delastic.apm.environment=production \
+  -Delastic.apm.application_packages=com.example.observability \
+  -Delastic.apm.trace_methods_duration_threshold=10ms \
+  -Delastic.apm.enable_metrics=true \
+  -Delastic.apm.log_level=INFO \
+  -jar /app/app.jar
